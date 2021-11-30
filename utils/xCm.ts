3678e143e7be8a0fb0fd7302b3682710afa991e8
leoksh <leo.kan@hthk.com>
2021-11-30 16:22:24 +0800

    - fix bug

--- utils/xCm.ts
+++ utils/xCm.ts
@@ -1,6 +1,3 @@
-import { Languangs } from 'types/i18n'
-import { Coordinate } from '../types/common'
-
 export const isBrowser = () => typeof window !== 'undefined'
 
 export const deviceType = () => {
@@ -21,34 +18,6 @@ export const deviceType = () => {
 export const isMobile = () => deviceType() === 'mobile'
 export const isTablet = () => deviceType() === 'mobile'
 
-export const getCurrentLocation = () =>
-  new Promise<Coordinate>((reslove, reject) => {
-    if (!navigator.geolocation) {
-      if (isMobile() || isTablet()) {
-        alert('Your browser does not support geolocation')
-      } else {
-        console.log('Your browser does not support geolocation')
-      }
-    } else {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          //   alert(`${position.coords.latitude},${position.coords.longitude}`)
-          reslove({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          })
-        },
-        error => {
-          if (isMobile() || isTablet()) {
-            alert(`getCurrentLocation error:\n${error.message}`)
-          } else {
-            console.log('getCurrentLocation error: ', error.message)
-          }
-        }
-      )
-    }
-  })
-
 export function contextLogger<T, P>(name = '', type: T, payload: P) {
   if (process.env.REACT_APP_ENV === 'PRODUCTION') return
   console.group(`${name} Action Detail`)
@@ -56,7 +25,3 @@ export function contextLogger<T, P>(name = '', type: T, payload: P) {
   console.log('Payload: ', payload)
   console.groupEnd()
 }
-
-export const getMemoAppLang = (): Languangs | null =>
-  (isBrowser() && (window.localStorage.getItem('appLanguage') as Languangs)) ||
-  null
