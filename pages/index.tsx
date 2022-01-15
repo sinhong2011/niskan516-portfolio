5a785ee32b02e8e19d2242ef8334947a94bbaefe
sinhong2011 <sinhong2011@gmail.com>
2022-01-15 22:42:00 +0800

    - update bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,19 +81,19 @@ function HomePage() {
         </Heading>
         <BioSection>
           <BioYear>2022</BioYear>
-          Studying Part-time course{' '}
-          <NextLink href="https://bit.ly/3FqTfil">
-            <Link href="https://bit.ly/3FqTfil" target={'_blank'}>
-              BCOMPHIT1 - BComp (Hons) Internet Tech
-            </Link>
-          </NextLink>
         </BioSection>
         <BioSection>
           <BioYear>2021</BioYear>
           Working at{' '}
           <NextLink href="/works/hthk/">
             <Link href="/works/hthk/">
-              Hutchison Telecom Hong Kong Holdings
+              Hutchison Telecom Hong Kong Holdings{' '}
+            </Link>
+          </NextLink>
+          and studying Part-time course{' '}
+          <NextLink href="https://bit.ly/3FqTfil">
+            <Link href="https://bit.ly/3FqTfil" target={'_blank'}>
+              BCOMPHIT1 - BComp (Hons) Internet Tech
             </Link>
           </NextLink>
         </BioSection>
